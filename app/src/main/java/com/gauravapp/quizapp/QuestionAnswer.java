90f648c073a0aa9f6614e4cb884b7501c7128d57
gaurav-savaimul <gauravvs4729@gmail.com>
2023-10-12 22:35:35 +0530

    final commit

---
+++ app/src/main/java/com/gauravapp/quizapp/QuestionAnswer.java
@@ -0,0 +1,28 @@
+package com.gauravapp.quizapp;
+
+public class QuestionAnswer {
+    public static String question[] ={
+            "Which company owns the android?",
+            "Which one is not the programming language?",
+            "Where you are watching this video?",
+            "Which company owns the Instagram?",
+            "Which programming language is used in Android Studio?"
+    };
+
+    public static String choices[][] = {
+            {"Google","Apple","Nokia","Samsung"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"Facebook","Whatsapp","Instagram","Youtube"},
+            {"Google","Apple","Meta","Twitter"},
+            {"Python","Go","Kotlin","HTML"}
+    };
+
+    public static String correctAnswers[] = {
+            "Google",
+            "Notepad",
+            "Youtube",
+            "Meta",
+            "Kotlin"
+    };
+
+}
